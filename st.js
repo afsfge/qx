7478340bf4c7fc13f874b4c0a188046f26971508
稻草人 <gecp@vip.qq.com>
2023-07-09 16:46:54 +0800

    更新 st.js

--- st.js
+++ st.js
@@ -1,6 +1,11 @@
-$httpClient.post({
+$httpClient.head({
   url: "http://cp.cloudflare.com/generate_204",
   policy:"🤡WRP-HK"
+});
+
+$httpClient.head({
+  url: "http://cp.cloudflare.com/generate_204",
+  policy:"🤡WRP-US"
 });
 console.log('请求成功！！！');
 $done();
