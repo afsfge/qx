2f6a55dc026b0f6d01b5e490beb6bbd40cbc64a6
稻草人 <gecp@vip.qq.com>
2023-07-09 16:40:36 +0800

    更新 st.js

--- st.js
+++ st.js
@@ -1,10 +1,6 @@
-$httpClient.get({
+$httpClient.post({
   url: "http://cp.cloudflare.com/generate_204",
   policy:"🤡WAP-HK"
-});
-$httpClient.get({
-  url: "http://cp.cloudflare.com/generate_204",
-  policy:"🤡WAP-US"
 });
 console.log('请求成功！！！');
 $done();
